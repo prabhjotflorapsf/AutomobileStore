707043aa3e88195051fbd772863040d9155a1f59
Pujitha Vadaparthy <30896589+PujithaVadaparthy@users.noreply.github.com>
2021-06-24 16:17:29 -0400

    test commit - toast

--- app/src/main/java/com/example/automobilestore/MainActivity.java
+++ app/src/main/java/com/example/automobilestore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.automobilestore;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show();
     }
 
 }

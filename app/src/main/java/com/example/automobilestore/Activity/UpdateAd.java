bb3d9d2183eec7ffdfcdd2d0bba423c73a1b97c9
pratit1997 <76486842+pratit1997@users.noreply.github.com>
2021-07-07 17:46:43 -0400

    layout Update ads

--- app/src/main/java/com/example/automobilestore/Activity/UpdateAd.java
+++ app/src/main/java/com/example/automobilestore/Activity/UpdateAd.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.automobilestore.R;
+
 public class UpdateAd extends AppCompatActivity {
 
     @Override

a8546245ed6d5addddaecbc1eedfacc9d1b58e87
sujaykumaryeddu <ysujaykumar@gmail.com>
2021-07-07 16:15:42 -0400

    Admin-Sign In

--- app/src/main/java/com/example/automobilestore/Activity/admin_signin.java
+++ app/src/main/java/com/example/automobilestore/Activity/admin_signin.java
@@ -1,32 +1,49 @@
 package com.example.automobilestore.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.automobilestore.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
 
 public class admin_signin extends AppCompatActivity {
 
     public TextInputLayout Email, Password;
     Button login;
     private FirebaseAuth auth;
+    FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_signin);
 
         Email = findViewById(R.id.admin_email);
         Password = findViewById(R.id.admin_password);
         login = findViewById(R.id.adminlogin);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_signin);
+        auth = FirebaseAuth.getInstance();
+
+        fStore = FirebaseFirestore.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +51,28 @@ public class admin_signin extends AppCompatActivity {
                 String email = Email.getEditText().getText().toString();
                 String pwd = Password.getEditText().getText().toString();
 
-                
+
+                Task<QuerySnapshot> documentReference = fStore.collection("Admin").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot doc:task.getResult()){
+                                String e = doc.getString("email");
+                                String p = doc.getString("password");
+                                if (e.equalsIgnoreCase(email) && p.equalsIgnoreCase(pwd)){
+                                    startActivity(new Intent(admin_signin.this,AdminHome.class));
+                                }else if(!e.equalsIgnoreCase(email)){
+                                    Email.requestFocus();
+                                    Email.setError("User Does not Exists with this email");
+                                }else if(!p.equalsIgnoreCase(pwd)){
+                                    Password.requestFocus();
+                                    Password.setError("Incorrect Password");
+                                }
+                            }
+                        }
+                    }
+                });
+
             }
         });
     }

4e91972acd8d2d79ecd552b5b30d90b730a2cbf9
prabhjotflorapsf <42036422+prabhjotflorapsf@users.noreply.github.com>
2021-07-08 17:58:45 -0400

    dialog box

---
+++ app/src/main/java/com/example/automobilestore/Activity/CarDetails.java
@@ -0,0 +1,17 @@
+package com.example.automobilestore.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.automobilestore.R;
+
+public class CarDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_car_details);
+    }
+}
---
+++ app/src/main/java/com/example/automobilestore/Activity/CarDialog.java
@@ -0,0 +1,14 @@
+package com.example.automobilestore.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_dialog);
+    }
+}

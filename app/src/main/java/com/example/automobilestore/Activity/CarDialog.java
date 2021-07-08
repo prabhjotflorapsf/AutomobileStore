0d815894c51c64a82e3a4b955d2da530fdae9d92
prabhjotflorapsf <42036422+prabhjotflorapsf@users.noreply.github.com>
2021-07-08 18:02:41 -0400

    Layout Car Info

--- app/src/main/java/com/example/automobilestore/Activity/CarDialog.java
+++ app/src/main/java/com/example/automobilestore/Activity/CarDialog.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.automobilestore.R;
+
 public class CarDialog extends AppCompatActivity {
 
     @Override

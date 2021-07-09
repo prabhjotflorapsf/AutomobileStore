79a29b9fb7121dd494651f1f984ee45ff2c577e1
Pujitha Vadaparthy <30896589+PujithaVadaparthy@users.noreply.github.com>
2021-07-09 16:17:05 -0400

    layout files
    reset and register

---
+++ app/src/main/java/com/example/automobilestore/admin_forgotpassword.java
@@ -0,0 +1,14 @@
+package com.example.automobilestore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class admin_forgotpassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_forgotpassword);
+    }
+}

bf00ce9d0daaaa2c0c0d8f348cbd4a3c9d0cbdea
yash14aug <yash.st16@gmail.com>
2021-07-21 22:54:19 -0400

    Edit User from Admin Panel

--- app/src/main/java/com/example/automobilestore/Admin/AdminHome.java
+++ app/src/main/java/com/example/automobilestore/Admin/AdminHome.java
@@ -1,32 +1,24 @@
 package com.example.automobilestore.Admin;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.automobilestore.Activity.PostAd;
-import com.example.automobilestore.Activity.ProfileDetails;
 import com.example.automobilestore.MainActivity;
 import com.example.automobilestore.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -41,7 +33,7 @@ public class AdminHome extends AppCompatActivity {
     private FirebaseAuth auth;
     FirebaseFirestore db;
     private FirebaseUser curUser;
-    LinearLayout addOns,Services;
+    LinearLayout addOns, services , userLogs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +46,8 @@ public class AdminHome extends AppCompatActivity {
         User_count=findViewById(R.id.tv_ucount);
         Post_count=findViewById(R.id.tv_pcount);
         addOns=findViewById(R.id.add_ons_btn);
-        Services=findViewById(R.id.services_btn);
+        services =findViewById(R.id.services_btn);
+        userLogs = findViewById(R.id.userLogs);
 
 //        User_count.setText(String.valueOf(getCount("User")+" User"));
 //       // Final_Count=0;
@@ -81,10 +74,17 @@ public class AdminHome extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        Services.setOnClickListener(new View.OnClickListener() {
+        services.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                Toast.makeText(AdminHome.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        userLogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(AdminHome.this , EditUser.class);
+                startActivity(i);
             }
         });
     }
---
+++ app/src/main/java/com/example/automobilestore/Admin/EditUser.java
@@ -0,0 +1,5 @@
+package com.example.automobilestore.Admin;
+
+public class EditUser {
+
+}
